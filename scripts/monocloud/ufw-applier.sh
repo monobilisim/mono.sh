acd4977041b12d1dd9db4f48679a52d66bd57db9
Kreato (Work) <kreato@kreato.dev>
2024-09-06 14:54:03 +0300

    ufw-applier: add static rules, fix /etc issues

--- scripts/monocloud/ufw-applier.sh
+++ scripts/monocloud/ufw-applier.sh
@@ -19,15 +19,18 @@ cron_mode "$ENABLE_CRON"
 
 function parse_config() {
     CONFIG_PATH_UFW="ufw"
-    export REQUIRED=true
 
+    export REQUIRED=true
     readarray -t RULE_URLS < <(yaml .rule_urls[] "$CONFIG_PATH_UFW")
+    
+    export REQUIRED=false
+    readarray -t STATIC_RULES < <(yaml .static_rules[] "$CONFIG_PATH_UFW")
 }
 
 function remove_file() {
-    protocol=$(awk '{print $1}' < /etc/mono.sh/ufw-applier-ruleset/"$(basename "$1")")
-    port=$(awk '{print $2}' < /etc/mono.sh/ufw-applier-ruleset/"$(basename "$1")")
-    description=$(cut -f3- -d ' ' < /etc/mono.sh/ufw-applier-ruleset/"$(basename "$1")")
+    protocol=$(awk '{print $1}' < /etc/mono/ufw-applier-ruleset/"$(basename "$1")")
+    port=$(awk '{print $2}' < /etc/mono/ufw-applier-ruleset/"$(basename "$1")")
+    description=$(cut -f3- -d ' ' < /etc/mono/ufw-applier-ruleset/"$(basename "$1")")
     echo "Removing old rulefile $1..."
     while IFS=: read -r line; do
         # Get IP address
@@ -63,12 +66,12 @@ function remove_file() {
 
 function apply_file() {
     echo "Processing $1..."
-    mkdir -p /etc/mono.sh/ufw-applier-ruleset
+    mkdir -p /etc/mono/ufw-applier-ruleset
 
     if [[ "$4" != "default" ]]; then
-        echo "$2 $3 $4" > /etc/mono.sh/ufw-applier-ruleset/"$(basename "$1")"
+        echo "$2 $3 $4" > /etc/mono/ufw-applier-ruleset/"$(basename "$1")"
     else
-        echo "$2 $3" > /etc/mono.sh/ufw-applier-ruleset/"$(basename "$1")"
+        echo "$2 $3" > /etc/mono/ufw-applier-ruleset/"$(basename "$1")"
     fi
 
     while IFS=: read -r line; do
@@ -106,6 +109,114 @@ function apply_file() {
     echo "Done processing $1"
 }
 
+function remove_file_static() {
+    IP=$(echo "$1" | awk '{print $1}')
+    PROTOCOL=$(echo "$1" | awk '{print $2}')
+    PORT=$(echo "$1" | awk '{print $3}')
+    DESCRIPTION=$(echo "$1" | cut -f4- -d ' ')
+    echo "Removing old rule $1..."
+    BASE_CMD="ufw delete allow"
+    if [[ "$PROTOCOL" == "all" ]]; then
+        BASE_CMD="$BASE_CMD from"
+    else
+        BASE_CMD="$BASE_CMD proto $PROTOCOL from"
+    fi
+
+    if [[ "$IP" == "all" ]]; then
+        BASE_CMD="$BASE_CMD any"
+    else
+        BASE_CMD="$BASE_CMD $IP"
+    fi
+
+    if [[ "$PORT" != "all" ]]; then
+        BASE_CMD="$BASE_CMD to any port $PORT"
+    fi
+
+    if [[ -n "$DESCRIPTION" ]]; then
+        BASE_CMD="$BASE_CMD comment '$DESCRIPTION'"
+    fi
+
+    [[ ! "$NO_CMD_OUT" == 1 ]] && echo "$BASE_CMD"
+    [[ ! "$DRY_RUN" == "1" ]] && eval "$BASE_CMD"
+}
+
+function apply_static() {
+    # syntax: IP PROTOCOL PORT DESCRIPTION
+    # eg. all tcp 25, all udp 25, all tcp 80, all udp 80
+    for rule in "${STATIC_RULES[@]}"; do
+        IP=$(echo "$rule" | awk '{print $1}')
+        PROTOCOL=$(echo "$rule" | awk '{print $2}')
+        PORT=$(echo "$rule" | awk '{print $3}')
+        DESCRIPTION=$(echo "$rule" | cut -f4- -d ' ')
+        
+        if [[ -f "/etc/mono/ufw-applier-ruleset/static-$IP-$PROTOCOL-$PORT" ]]; then
+            return 
+        fi
+
+        BASE_CMD="ufw allow"
+        if [[ "$PROTOCOL" == "all" ]]; then
+            BASE_CMD="$BASE_CMD from"
+        else
+            BASE_CMD="$BASE_CMD proto $PROTOCOL from"
+        fi
+
+        if [[ "$IP" == "all" ]]; then
+            BASE_CMD="$BASE_CMD any"
+        else
+            BASE_CMD="$BASE_CMD $IP"
+        fi
+
+        if [[ "$PORT" != "all" ]]; then
+            BASE_CMD="$BASE_CMD to any port $PORT"
+        fi
+
+        if [[ -n "$DESCRIPTION" ]]; then
+            BASE_CMD="$BASE_CMD comment '$DESCRIPTION'"
+        fi
+       
+        echo "$rule" > /etc/mono/ufw-applier-ruleset/static-"$IP"-"$PROTOCOL"-"$PORT"
+        [[ ! "$NO_CMD_OUT" == 1 ]] && echo "$BASE_CMD"
+        [[ ! "$DRY_RUN" == "1" ]] && eval "$BASE_CMD"
+
+    done
+}
+
+function chkandremove_static() {
+    FILES=()
+    for file in /etc/mono/ufw-applier-ruleset/static-*; do
+        for rule in "${STATIC_RULES[@]}"; do
+            IP=$(echo "$rule" | awk '{print $1}')
+            PROTOCOL=$(echo "$rule" | awk '{print $2}')
+            PORT=$(echo "$rule" | awk '{print $3}')
+            DESCRIPTION=$(echo "$rule" | cut -f4- -d ' ')
+            FILES+=("static-$IP-$PROTOCOL-$PORT")
+            if [[ "$(basename "$file")" == "static-$IP-$PROTOCOL-$PORT" ]]; then
+                SHA256=$(sha256sum "$file" | awk '{print $1}')
+                SHA256SUM_ORIG=$(echo "$rule" | sha256sum | awk '{print $1}')
+                if [[ "$SHA256" != "$SHA256SUM_ORIG" ]]; then
+                    echo "Removing $file (sum mismatch)"
+                    remove_file_static "$(cat "$file")"
+                    rm -f "$file"
+                fi
+            fi
+        done
+    done
+
+    for file in /etc/mono/ufw-applier-ruleset/static-*; do
+        if [[ ! -f "$file" ]]; then
+            continue
+        fi
+
+        if is_in_array "$(basename "$file")" "${FILES[@]}"; then
+            continue
+        fi
+            
+        echo "Removing $file (not in static rules)"
+        remove_file_static "$(cat "$file")"
+        rm -f "$file"
+    done
+}
+
 is_in_array() {
     local value=$1
     local array=$2
@@ -118,7 +229,7 @@ is_in_array() {
 }
 
 main() {
-    mkdir /etc/mono.sh/ufw-applier/ &> /dev/null
+    mkdir /etc/mono/ufw-applier/ &> /dev/null
     parse_config
     FILES_PROCESSED=()
     for rule in "${RULE_URLS[@]}"; do
@@ -128,9 +239,9 @@ main() {
         rule_port=$(echo "$rule" | awk '{print $3}')
         rule_description=$(echo "$rule" | cut -f4- -d ' ')
         curl -s -o "$TMP_PATH_SCRIPT/$rule_file-tmp" "$rule_url"
-        if [[ -f "/etc/mono.sh/ufw-applier/$rule_file" ]]; then
-            SUM_ORIG=$(sha256sum "/etc/mono.sh/ufw-applier/$rule_file" | awk '{print $1}')
-            PORTFILE=$(cat "/etc/mono.sh/ufw-applier-ruleset/$rule_file")
+        if [[ -f "/etc/mono/ufw-applier/$rule_file" ]]; then
+            SUM_ORIG=$(sha256sum "/etc/mono/ufw-applier/$rule_file" | awk '{print $1}')
+            PORTFILE=$(cat "/etc/mono/ufw-applier-ruleset/$rule_file")
 
             PORTFILE_NEW="$rule_protocol $rule_port"
 
@@ -140,17 +251,17 @@ main() {
 
             if [[ $(sha256sum "$TMP_PATH_SCRIPT/$rule_file-tmp" | awk '{print $1}') != "$SUM_ORIG" || "$PORTFILE" != "$PORTFILE_NEW" ]]; then
                 echo "Sum mismatch, updating $rule_file"
-                remove_file "/etc/mono.sh/ufw-applier/$rule_file"
-                mv "$TMP_PATH_SCRIPT/$rule_file-tmp" "/etc/mono.sh/ufw-applier/$rule_file"
-                apply_file "/etc/mono.sh/ufw-applier/$rule_file" "${rule_protocol:-tcp}" "${rule_port:?no port specified}" "${rule_description:-default}"
+                remove_file "/etc/mono/ufw-applier/$rule_file"
+                mv "$TMP_PATH_SCRIPT/$rule_file-tmp" "/etc/mono/ufw-applier/$rule_file"
+                apply_file "/etc/mono/ufw-applier/$rule_file" "${rule_protocol:-tcp}" "${rule_port:?no port specified}" "${rule_description:-default}"
             else
                 echo "No sum mismatch, no change necessary"
                 rm -f "$TMP_PATH_SCRIPT/$rule_file-tmp" # No change necessary
             fi
         else
             echo "No existing rule file, creating $rule_file"
-            mv "$TMP_PATH_SCRIPT/$rule_file-tmp" "/etc/mono.sh/ufw-applier/$rule_file"
-            apply_file "/etc/mono.sh/ufw-applier/$rule_file" "${rule_protocol:-tcp}" "${rule_port:?no port specified}" "${rule_description:-default}"
+            mv "$TMP_PATH_SCRIPT/$rule_file-tmp" "/etc/mono/ufw-applier/$rule_file"
+            apply_file "/etc/mono/ufw-applier/$rule_file" "${rule_protocol:-tcp}" "${rule_port:?no port specified}" "${rule_description:-default}"
         fi
 
         FILES_PROCESSED+=("$rule_file")
@@ -158,7 +269,12 @@ main() {
 
     echo "Files processed: '${FILES_PROCESSED[*]}'"
 
-    for file in /etc/mono.sh/ufw-applier/*; do
+    for file in /etc/mono/ufw-applier/*; do
+        # Skip if static rule
+        if basename "$file" | grep -qE '^static-.*'; then
+            continue
+        fi
+
         for rule_file in "${FILES_PROCESSED[@]}"; do
             if [[ "$(basename "$file")" == "$rule_file" ]]; then
                 continue 2
@@ -168,6 +284,9 @@ main() {
         remove_file "$file"
         rm -f "$file"
     done
+
+    chkandremove_static
+    apply_static
 }
 
 main
